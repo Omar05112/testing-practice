b3eb1a88a26fe937cf97b72ea0282feea7c10957
Omar05112 <omarhamaini1@gmail.com>
2026-03-19 20:38:55 +0000

    Add capitalize function with working test.

---
+++ src/capitalize.js
@@ -0,0 +1,3 @@
+export const capitalize = (val) =>{
+  return String(val).toUpperCase();
+};
---
+++ src/capitalize.test.js
@@ -0,0 +1,6 @@
+import expect from "expect";
+import { capitalize } from "./capitalize.js";
+
+test("String should be capitalized", () => {
+  expect(capitalize("tekken")).toBe("TEKKEN");
+});
